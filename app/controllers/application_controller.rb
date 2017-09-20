f4a8dd0bd37ea48adcac427a818ebedebb69b546
r624009 <alexander.thorpe@cambiahealth.com>
2017-09-19 19:41:52 -0700

    added category navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_navigation
+
+  private
+    def set_navigation
+      @nav_categories = Category.order('lft ASC')
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   def index
     @features = Feature.all
     @services = Service.where(homepage: true)
+    @categories = Category.where(depth: 0)
   end
 
   def admin
